67b9d282a538f368ded00389dd532c3d0206541d
enzogbiya <lipnitskiy.kirill@mail.ru>
2022-12-29 12:29:34 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
-  );
-}
+import * as faceapi from "face-api.js";
+
+const App = () => {
+	const inputField = useRef(null);
+
+	useEffect(() => {
+		Promise.all([
+			faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+			faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+			faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+		]).then(start);
+	}, []);
+
+	const start = async () => {
+		console.log("Loaded");
+	};
+
+	return (
+		<div className="App">
+			<input type="file" ref={inputField}></input>
+		</div>
+	);
+};
 
 export default App;
